147533fb9a7c263997c3c5db44fb4135100a72e7
karljackab <karljackab@gmail.com>
2018-03-17 18:18:26 +0800

    update findStudent

--- src/sqlString.js
+++ src/sqlString.js
@@ -2,7 +2,13 @@ exports.findStudent = "\
     select s.student_id,s.sname,s.program,s.grade,s.email,s.graduate,s.graduate_submit,\
     s.gmail,s.fb_id,s.github_id,e.pass_code as en_certificate\
     from student as s,en_certificate as e\
-    where s.student_id=:id and e.student_id=:id";
+    where s.student_id=:id and e.student_id=:id\
+    union\
+    select s.student_id,s.sname,s.program,s.grade,s.email,s.graduate,s.graduate_submit,\
+    s.gmail,s.fb_id,s.github_id,NULL as en_certificate\
+    from student as s\
+    where s.student_id=:id and s.student_id not in\
+    (select student_id from en_certificate)";
 
 exports.findProfessor = "\
     select teacher_id,tname from teacher\
