748bdac3528da39fcb94984d2e222956383d2c7c
ccc870206 <ccc870206@gmail.com>
2018-07-31 02:04:55 +0800

    complete ShowTeacherIdList

--- src/Teacher/teacher_sqlString.js
+++ src/Teacher/teacher_sqlString.js
@@ -49,8 +49,55 @@ exports.ShowTeacherMentors ="\
     and m.student_id = s.student_id";
 
 exports.ShowTeacherIdList="\
-    select t.teacher_id, t.tname, ti.email, count(m.student_id) as all_student\
-    from teacher as t, teacher_info as ti,mentor_list as m\
-    where t.tname = ti.tname\
-    and t.tname = m.tname\
-    group by t.tname";
+    select\
+        f.teacher_id as id, f.tname as name, f.email as email, a.all_students, f.failed_students\
+    from\
+    (\
+        select t.teacher_id, t.tname, ti.email, COUNT(final.failed) as failed_students\
+        from teacher as t\
+        left outer join\
+        (\
+            select DISTINCT\
+                (if(failed, s_final.student_id, '')) as failed, s_final.tname\
+            from\
+            (\
+                select\
+                    m.tname, s.student_id, concat(s.cos_year, '-', s.semester) as sem,\
+                    if(sum(if(s.pass_fail = '不通過', cd.cos_credit, 0)*2 )\
+                    >= sum(if(1, cd.cos_credit, 0)), 1, 0) as failed\
+                from\
+                (\
+                    select s.student_id, cs.pass_fail, cs.cos_year, cs.semester, cs.cos_id\
+                    from student as s, cos_score as cs\
+                    where s.student_id = cs.student_id\
+                    and ( cs.pass_fail = '通過' or cs.pass_fail = '不通過')\
+                )as s,\
+                cos_data as cd, mentor_list as m\
+                where cd.unique_id = concat(s.cos_year, '-', s.semester, '-', s.cos_id)\
+                and m.student_id = s.student_id\
+                group by s.student_id, concat(s.cos_year, '-', s.semester)\
+            )as s_final\
+            where failed != ''\
+            group by s_final.student_id, s_final.sem\
+        ) as final\
+        on t.tname = final.tname\
+        left outer join\
+        teacher_info as ti\
+        on t.tname = ti.tname\
+        group by t.tname\
+    ) as f\
+    inner join\
+    (\
+        select\
+            t.teacher_id, t.tname, ti.email, count(m.student_id) as all_students\
+        from\
+        (teacher as t\
+        left outer join\
+        teacher_info as ti\
+        on t.tname = ti.tname)\
+        left outer join\
+        mentor_list as m\
+        on t.tname = m.tname\
+        group by t.tname\
+    )as a\
+    on f.tname = a.tname";
