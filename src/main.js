a72d620d7c16f9a9cef02c7462e281b9d083894f
ccc870206 <ccc870206@gmail.com>
2019-02-13 20:50:22 +0800

    modify SetOffsetApplyFormAgreeStatus state definition

--- src/main.js
+++ src/main.js
@@ -389,36 +389,36 @@ var m = require('./msql.js');
 // // 修改專題資料的 add_status, 0代表尚未加選 1代表已加選
 
 
-// data = { 
-// 	student_id: '0516005',
-// 	phone: '0123456789',
-// 	apply_year: '107',
-// 	apply_semester: '1',
-// 	cos_dep_old: '電子系',
-// 	cos_tname_old: '桑梓賢',
-// 	cos_cname_old: '線性',
-// 	cos_code_old:  null,
-// 	cos_cname: '線性代數',
-// cos_code: 'DCP2354',
-// cos_type: '必修',
-// reason: 'YAAAA YOOOO YEEEE',
-// credit_old: 3,
-// file: 'LOOOOOOOOOO',
-// school_old: 'NTCU',           
-// dep_old: '應數系',              
-// graduation_credit_old: 128,
-// cos_year_old: 106,         
-// cos_semester_old: 2,     
-// score_old: 92,
-// offset_type: 2
-// };
+data = { 
+	student_id: '0516005',
+	phone: '0123456789',
+	apply_year: '107',
+	apply_semester: '1',
+	cos_dep_old: '電子系',
+	cos_tname_old: '桑梓賢',
+	cos_cname_old: '線性',
+	cos_code_old:  null,
+	cos_cname: '線性代數',
+cos_code: 'DCP2354',
+cos_type: '必修',
+reason: 'YAAAA YOOOO YEEEE',
+credit_old: 3,
+file: 'LOOOOOOOOOO',
+school_old: 'NTCU',           
+dep_old: '應數系',              
+graduation_credit_old: 128,
+cos_year_old: 106,         
+cos_semester_old: 2,     
+score_old: 92,
+offset_type: 2
+};
 
-// m.CreateOffsetApplyForm(data,function(err,result){
-// 	if(err)
-// 		throw err;
-// 	console.log(JSON.parse(result));
-// }); // 	建立課程抵免申請單，回傳對DB造成的info 
-// 	   //	offset_type -> 0 外系抵免 1 英授抵免 2 免修單  3 學分抵免單
+m.CreateOffsetApplyForm(data,function(err,result){
+	if(err)
+		throw err;
+	console.log(JSON.parse(result));
+}); // 	建立課程抵免申請單，回傳對DB造成的info 
+	   //	offset_type -> 0 外系抵免 1 英授抵免 2 免修單  3 學分抵免單
 
 
 
@@ -453,13 +453,13 @@ var m = require('./msql.js');
 // }); // create offset
 
 
-data1 = {student_id: '0516005'} // return single student offset apply form
-data2 = {all_student: true} // return all student offset apply form
-m.ShowUserOffsetApplyForm(data1,function(err,result){
-	if(err)
-		throw err;
-	console.log(JSON.parse(result));
-});	
+// data1 = {student_id: '0516005'} // return single student offset apply form
+// data2 = {all_student: true} // return all student offset apply form
+// m.ShowUserOffsetApplyForm(data1,function(err,result){
+// 	if(err)
+// 		throw err;
+// 	console.log(JSON.parse(result));
+// });	
 
 // data = {
 // 	student_id: '0516003',
@@ -475,7 +475,7 @@ m.ShowUserOffsetApplyForm(data1,function(err,result){
 // data = {
 // 	timestamp: '2019-02-13 13:17:19',
 // 	student_id: '0516005',
-// 	state: 1 ,// 0 尚未決定，1 助理同意，2 主任同意，3 助理不同意，4 主任不同意
+// 	state: 1 ,// 0 申請中，1 同意抵免，2 等候主管同意，3 等候老師同意，4 抵免失敗，5 退回等學生修改 
 // 	reject_reason: '被退回的原因',
 // 	transferto: '["T9505","T9125"]'
 // }
