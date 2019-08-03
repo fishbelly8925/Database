3f97e07c78d88737496714c35ab86ecf742e03e3
yao0510 <sf163864@gmail.com>
2019-08-04 00:01:33 +0800

    modify commit on ShowUserDefined

--- src/main_chatbot.js
+++ src/main_chatbot.js
@@ -4,10 +4,10 @@
 
 var m = require('./msql.js');
 
-m.ShowUserDefined('all',function(err, result){
+m.ShowUserDefined('化學（一）',function(err, result){
     if(err)
         throw err;
     console.log(JSON.parse(result));
-}); // ShowAllUserDefined 列出所有自定義 (輸入all顯示全部)
+}); // ShowAllUserDefined 列出特定自定義 (輸入all顯示全部)
 
 m.Drain(); // 關閉 connection pool
