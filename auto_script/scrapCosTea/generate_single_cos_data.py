c22ac84dac59abf1ed7bfcc5eaf74e4e1e6ca267
karljackab <karljackab@gmail.com>
2019-08-25 22:17:56 +0800

    backup recover

--- auto_script/scrapCosTea/generate_single_cos_data.py
+++ auto_script/scrapCosTea/generate_single_cos_data.py
@@ -6,15 +6,17 @@
 import csv
 import sys
 
+base_dir = '/home/nctuca/db_data/scrapCosTea/'
+
 dtHeader = ['unique_id','brief','brief_new','cos_code','cos_credit','cos_hours','cos_id','cos_time','cos_type','cos_typeext','degree','depType','dep_id','memo','num_limit','reg_num','teacher','grade']
 cnHeader = ['unique_id','cos_code','cos_cname','cos_ename']
 filename = sys.argv[1]
 
-cn = open('cos_name.csv','w')
+cn = open(base_dir+'cos_name.csv','w')
 cnWriter = csv.writer(cn)
 cnWriter.writerow(cnHeader)
 
-dt = open('cos_data.csv', 'w')
+dt = open(base_dir+'cos_data.csv', 'w')
 dtWriter = csv.writer(dt)
 dtWriter.writerow(dtHeader)
 
@@ -24,20 +26,20 @@ cdReader = csv.reader(cd)
 next(cdReader, None)  # skip the headers
 
 for row in cdReader:
-	unique_id, cos_code = row[0].strip('"'), row[4].strip('"')
-	cos_cname, cos_ename = row[3].strip('"'), row[6].strip('"')
-	cnWriter.writerow([unique_id, cos_code, cos_cname, cos_ename,''])
+    unique_id, cos_code = row[0].strip('"'), row[4].strip('"')
+    cos_cname, cos_ename = row[3].strip('"'), row[6].strip('"')
+    cnWriter.writerow([unique_id, cos_code, cos_cname, cos_ename,''])
 
-	brief, brief_new = row[1], row[2]
-	cos_credit, cos_hours = row[5], row[7] 
-	cos_id, cos_time = row[8], row[9]
-	cos_type, cos_typeext = row[10], row[11]
-	degree, depType = row[12], row[13]
-	dep_id, memo = row[14], row[15]
-	num_limit, reg_num = row[16], row[17]
-	teacher, grade = row[18], row[19]
+    brief, brief_new = row[1], row[2]
+    cos_credit, cos_hours = row[5], row[7] 
+    cos_id, cos_time = row[8], row[9]
+    cos_type, cos_typeext = row[10], row[11]
+    degree, depType = row[12], row[13]
+    dep_id, memo = row[14], row[15]
+    num_limit, reg_num = row[16], row[17]
+    teacher, grade = row[18], row[19]
 
-	dtWriter.writerow([unique_id,brief,brief_new,cos_code,cos_credit,cos_hours,cos_id,cos_time,cos_type,cos_typeext,degree,depType,dep_id,memo,num_limit,reg_num,teacher,grade,''])
+    dtWriter.writerow([unique_id,brief,brief_new,cos_code,cos_credit,cos_hours,cos_id,cos_time,cos_type,cos_typeext,degree,depType,dep_id,memo,num_limit,reg_num,teacher,grade,''])
 
 cd.close()
 dt.close()
---
+++ auto_script/scrapCosTea/update_cos_data.sql
@@ -0,0 +1,20 @@
+load data local infile '/home/nctuca/db_data/scrapCosTea/cos_data.csv'
+into table cos_data
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile '/home/nctuca/db_data/scrapCosTea/cos_name.csv'
+into table cos_name
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+ignore 1 lines;
+
+load data local infile '/home/nctuca/db_data/scrapCosTea/TeacherList.csv'
+into table teacher
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+ignore 1 lines;
