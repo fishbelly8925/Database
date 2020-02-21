c51603079c62504cfc2d749de0324e9f30146be3
yao0510 <sf163864@gmail.com>
2020-02-21 17:38:24 +0800

    use temporary table to insert

--- python/automation/insert_on_cos_data.py
+++ python/automation/insert_on_cos_data.py
@@ -33,7 +33,7 @@ def validateCSV(file_path):
             return validate_flag              
     return validate_flag
 
-def deletelastcourse(mycursor, connection):
+def deleteLastCourse(mycursor, connection):
     record_status = None
     code = None
     message = None
@@ -54,22 +54,70 @@ def insertDB(file_path, mycursor, connection):
     record_status = None
     code = None
     message = None
-    sql = """LOAD DATA LOCAL INFILE '{}'
-            INTO TABLE on_cos_data
-            CHARACTER SET 'utf8'
-            FIELDS TERMINATED BY ','
-            ENCLOSED BY '"'
-            LINES TERMINATED BY '\n'
-            IGNORE 1 LINES;"""
+    affect_count = None
+
+    dump_sql1 = '''
+        create temporary table temp_on_cos_data(
+            student_id varchar(20),
+            year int(11),
+            semester int(11),
+            code varchar(10),
+            cos_dep varchar(20),
+            cos_code varchar(20),
+            cos_type varchar(20),
+            scr_summaryno varchar(10),
+            cos_credit float,
+            primary key(student_id, year, semester, code)
+        )DEFAULT CHARSET=utf8mb4;
+    '''
+    dump_sql2 = '''
+        load data local infile '{}'
+        into table temp_on_cos_data
+        character set 'utf8'
+        fields terminated by ','
+        enclosed by '"'
+        lines terminated by '\n'
+        ignore 1 lines;
+    '''
+    dump_sql3 = '''
+        insert into on_cos_data
+        select * from temp_on_cos_data
+        on duplicate key update
+        cos_dep = values(cos_dep), 
+        cos_code = values(cos_code),
+        cos_type = values(cos_type),
+        scr_summaryno = values(scr_summaryno),
+        cos_credit = values(cos_credit)
+        ;
+    '''
+    dump_sql4 = '''
+        drop temporary table temp_on_cos_data;
+    '''
+
+    # try:
+    #     affect_count = mycursor.execute(sql.format(file_path))
+    #     connection.commit()
+    #     record_status = 1
+    # except pymysql.InternalError as error:
+    #     code, message = error.args
+    #     record_status = 0
+    #     # Rollback in case there is any error
+    #     # connection.rollback()
+
     try:
-        affect_count = mycursor.execute(sql.format(file_path))
-        connection.commit()
-        record_status = 1
+        mycursor.execute(dump_sql1)
+        mycursor.execute(dump_sql2.format(file_path))
+        affect_count = mycursor.execute(dump_sql3)
+        mycursor.execute(dump_sql4)
     except pymysql.InternalError as error:
         code, message = error.args
         record_status = 0
-        # Rollback in case there is any error
-        # connection.rollback()
+        connection.rollback()
+    else:
+        print('Success')
+        connection.commit()
+        record_status = 1
+
     return record_status, code, message, affect_count
 
 if __name__ == '__main__':
@@ -85,7 +133,7 @@ if __name__ == '__main__':
 
     if validate_flag == True:
         #Delete last semester's on cos data
-        record_status, code, message, affect_count = deletelastcourse(mycursor, connection)
+        record_status, code, message, affect_count = deleteLastCourse(mycursor, connection)
         if record_status == 0:
             message = "刪除上學期課程錯誤：" + message
             checkFile.recordLog(calling_file, record_status, message, mycursor, connection)
