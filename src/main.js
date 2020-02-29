4ee61a8536089b5ac5077ebaea522979d9c4c504
karljackab <karljackab@gmail.com>
2020-03-01 00:32:16 +0800

    change InsertNewData to transfer version

--- src/main.js
+++ src/main.js
@@ -709,13 +709,13 @@ var m = require('./msql.js');
 //     console.log(JSON.parse(result));
 // });
 
-// m.InsertNewData({file_name: '108-2_on_cos_data.csv', data_type: "當期修課資料", semester: '108-2'});
-// // 執行自動化匯入，因為是offline的作法，所以沒有call back function
-// // 參數：
-// // file_name: 檔案名稱
-// // data_type:
-// //      "課程成績資料", "新老師資料", "當期修課資料", "學生資料", "抵免資料", "英文換修資料"
-// // semester: "108-1", "108-2", "109-1" ... 
+m.InsertNewData({file_name: '學生資料範例.xlsx', data_type: "學生資料", semester: '108-2'});
+// 執行自動化匯入，因為是offline的作法，所以沒有call back function
+// 參數：
+// file_name: 檔案名稱
+// data_type:
+//      "課程成績資料", "新老師資料", "當期修課資料", "學生資料", "抵免資料", "英文換修資料"
+// semester: "108-1", "108-2", "109-1" ... 
 
 // m.ShowAllDataLog(function(err, result){
 //     if(err)
