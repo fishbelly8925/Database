7f1904c7708ecd5ec61fb8dbc65f2be183e44264
karljackab <karljackab@gmail.com>
2020-05-03 18:18:02 +0800

    update automation script

---
+++ auto_script/sync_db.sh
@@ -0,0 +1,3 @@
+mysqldump -uroot -pHahaha ca > /home/nctuca/dinodino-extension/coverca_test.sql
+mysql -uroot -pHahaha ca_test < /home/nctuca/dinodino-extension/coverca_test.sql
+rm /home/nctuca/dinodino-extension/coverca_test.sql
